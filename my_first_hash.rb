7eace85b3f3e9aef6d65c60951a38b49ee035287
Taylor Sieling <taylorasieling@gmail.com>
2020-08-28 23:59:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,12 +7,7 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
  shipping_manifest["oil paintings"]
 end
@@ -23,14 +18,14 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+      shipping_manifest["muskets"] = 2
+      
   # add 4 gun powder to the shipping_manifest hash below
-
-
+      shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+      shipping_manifest
+      
 end
