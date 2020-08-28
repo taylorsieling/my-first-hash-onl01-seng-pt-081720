ec5823763876b75e72e3e6fbca587ddefe2845f6
Taylor Sieling <taylorasieling@gmail.com>
2020-08-28 23:52:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_favorites = {"book" => "Harry Potter", "desert" => "cheesecake", "beverage" => "hot tea", "language" => "ruby"}
 end
 
 
